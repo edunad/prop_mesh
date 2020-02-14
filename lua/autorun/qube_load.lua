be767d86852c33a256fc562993bb6d3180c93766
failcake <failcake.work@gmail.com>
2020-02-14 13:34:02 +0000

    Fixed gmad.exe errors

--- lua/autorun/qube_load.lua
+++ lua/autorun/qube_load.lua
@@ -6,14 +6,14 @@ if SERVER then
 	-- LIB --
 	-- Client side --
 	AddCSLuaFile("lib/cl/setup.lua")
-	AddCSLuaFile("lib/cl/PVSCache.lua")
-	AddCSLuaFile("lib/cl/URLTexture.lua")
+	AddCSLuaFile("lib/cl/pvs_cache.lua")
+	AddCSLuaFile("lib/cl/url_texture.lua")
 	
 	-- Shared --
 	AddCSLuaFile("lib/sh/setup.lua")
-	AddCSLuaFile("lib/sh/meshParser.lua")
+	AddCSLuaFile("lib/sh/mesh_parser.lua")
 	AddCSLuaFile("lib/sh/obj.lua")
-	AddCSLuaFile("lib/sh/queueSYS.lua")
+	AddCSLuaFile("lib/sh/queue_sys.lua")
 	AddCSLuaFile("lib/sh/util.lua")
 end
 
@@ -29,15 +29,15 @@ end
 -- CLIENT --
 if CLIENT then
 	include("lib/cl/setup.lua")
-	include("lib/cl/PVSCache.lua")
-	include("lib/cl/URLTexture.lua")
+	include("lib/cl/pvs_cache.lua")
+	include("lib/cl/url_texture.lua")
 end
 
 -- SHARED --
 include("lib/sh/setup.lua")
-include("lib/sh/meshParser.lua")
+include("lib/sh/mesh_parser.lua")
 include("lib/sh/obj.lua")
-include("lib/sh/queueSYS.lua")
+include("lib/sh/queue_sys.lua")
 include("lib/sh/util.lua")
 
 if SERVER then print("[QUBELib] Startup") end
